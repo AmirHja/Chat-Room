f8efcd7ca5541269a121098767ba5120d8e481f9
Amirparsa Hajiabdollah <amirhja82@gmail.com>
2025-03-20 01:11:11 +0330

    User log Forms added
    - Login/Sign Up Form added

---
+++ users/forms.oy.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth import authenticate
+from .models import User
+
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords do not match!")
+
+        return cleaned_data
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError("Invalid username or password")
+
+        return cleaned_data
