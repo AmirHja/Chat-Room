cdb1744f14b1c2184fa60f28829d64426ead82c1
Amirparsa Hajiabdollah <amirhja82@gmail.com>
2025-03-19 23:39:00 +0330

    User models added
    - User app created
    - User models created
    - Settings configs are updated

---
+++ users/models.py
@@ -0,0 +1,106 @@
+import random
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core import validators
+from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager)
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, is_staff, is_supper, **extra_fields):
+        """
+        Create and save a user with the given username and password.
+        """
+        now = timezone.now()
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(
+            username=username,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_supper,
+            date_joined=now,
+            **extra_fields
+        )
+
+        if not extra_fields.get('no password'):
+            user.set_password(password)
+
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username=None, password=None, **extra_fields):
+        if username is None:
+            username = 'user@'
+            while User.objects.filter(username=username).exists():
+                username = username.split('@')[0] + "@"
+                for _ in range(8):
+                    username += str(random.randint(10, 99))
+
+        return self._create_user(username, password, is_staff=False, is_supper=False, **extra_fields)
+
+
+    def create_superuser(self, username=None, password=None, **extra_fields):
+        if username is None:
+            username = 'user@'
+            while User.objects.filter(username=username).exists():
+                username = username.split('@')[0] + "@"
+                for _ in range(8):
+                    username += str(random.randint(10, 99))
+
+        return self._create_user(username, password, is_staff=True, is_supper=True, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username, password and email are required. Other fields are optional.
+    """
+    username = models.CharField(
+        verbose_name=_('username'),
+        max_length=32,
+        unique=True,
+        help_text="Required. 4 to 32 characters. Letters, digits and @/./_ only. Starts with a letter",
+        validators=[
+            validators.RegexValidator(
+                regex=r"^[a-zA-Z][a-zA-Z0-9@._]{3,}$",
+                message=_("Enter a valid username."),
+                code="Invalid username"
+            )
+        ],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        }
+    )
+    is_staff = models.BooleanField(
+        verbose_name=_('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.')
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('active status'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active.'
+            'Unselect this instead of deleting accounts.'
+        )
+    )
+    date_joined = models.DateTimeField(verbose_name=_('date joined'), default=timezone.now)
+    last_seen = models.DateTimeField(verbose_name=_('last seen'), null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
