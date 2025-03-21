3c7b9b61d7c6c11aa9b6d1ad71ac757c942c16c2
Amirparsa Hajiabdollah <amirhja82@gmail.com>
2025-03-21 18:27:52 +0330

    Forbidden page created
    - A forbidden page was designed to prevent non-authorised users

--- chats/urls.py
+++ chats/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import chat_room
 
 urlpatterns = [
+
     path("<str:room_name>/", chat_room, name="chat_room"),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,14 +1,18 @@
+from django.http.response  import HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Message, Room
 
 
-@login_required
-def chat_room(request, room_name):
-    room = get_object_or_404(Room, name=room_name)
-    messages = Message.objects.filter(room=room).order_by("created_at")
 
-    return render(request, "chats/chat_room.html", {
-        "room_name": room.name,
-        "messages": messages
-    })
+def chat_room(request, room_name):
+    if request.user.is_authenticated:
+        room = get_object_or_404(Room, name=room_name)
+        messages = Message.objects.filter(room=room).order_by("created_at")
+
+        return render(request, "chats/chat_room.html", {
+            "room_name": room.name,
+            "messages": messages
+        })
+    else:
+        return render(request, 'chats/forbidden.html')
