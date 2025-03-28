825ac08aae5334dbbae87205401dbc05dc367a5d
Amirparsa Hajiabdollah <amirhja82@gmail.com>
2025-03-28 22:39:31 +0330

    Add logs for server
    - Add some Logs for server
    - Add new styles for templates

--- chats/views.py
+++ chats/views.py
@@ -13,41 +13,50 @@ def public_message(request):
         room = get_object_or_404(Room, name=room_name)
         messages = PublicMessage.objects.filter(room=room).order_by("created_at")
 
-        return render(request, "chats/public_message.html", {
-            "room_name": room.name,
-            "messages": messages
-        })
+        print(f"<{request.user.username}> join the chat room.")
+        print(f"Hi <{request.user.username}>, welcome to the chat room.")
+        return render(
+            request=request,
+            template_name="chats/public_message.html",
+            context={
+                "room_name": room.name,
+                "messages": messages
+            }
+        )
     else:
-        return render(request, 'chats/forbidden.html')
+        return render(request=request, template_name='chats/forbidden.html')
 
 def private_message(request):
     if request.user.is_authenticated:
         user = request.user
-        chat_partners = User.objects.filter(
+        chat_users = User.objects.filter(
             id__in=PrivateMessage.objects.filter(receiver=user).values_list('sender', flat=True)
         ) | User.objects.filter(
             id__in=PrivateMessage.objects.filter(sender=user).values_list('receiver', flat=True)
         )
-        return render(request, "chats/private_message.html", {"chat_partners": chat_partners})
+        return render(request, "chats/private_message.html",{"chat_users": chat_users})
     else:
         return render(request, 'chats/forbidden.html')
 
 def load_message(request, username):
     if request.user.is_authenticated:
         user = request.user
-        partner = get_object_or_404(User, username=username)
+        receiver = get_object_or_404(User, username=username)
 
         messages = PrivateMessage.objects.filter(
-            sender__in=[user, partner],
-            receiver__in=[user, partner]
-        ).order_by("created_at")
+            sender=user, receiver=receiver
+        ) | PrivateMessage.objects.filter(
+            sender=receiver, receiver=user
+        )
 
-        messages_data = [
-            {"sender": msg.sender.username, "message": msg.message,
-             "timestamp": msg.created_at.strftime("%Y-%m-%d %H:%M")}
-            for msg in messages
-        ]
-        return JsonResponse({"messages": messages_data})
+        messages = messages.order_by("created_at")
+
+        return JsonResponse({
+            "messages": [
+                {"sender": msg.sender.username, "message": msg.message, "timestamp": msg.created_at}
+                for msg in messages
+            ]
+        })
     else:
         return render(request, 'chats/forbidden.html')
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
+import os
+import hashlib
+from Crypto.Cipher import AES
+import base64
+
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
@@ -12,7 +18,8 @@ def signup_view(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            login(request, user)
+            print(f"Registration <{user.username}> <{form.cleaned_data['password']}>")
+            # login(request, user)
             return redirect('login')
     else:
         form = SignUpForm()
@@ -22,6 +29,7 @@ def signup_view(request):
 
 def login_view(request):
     if request.method == "POST":
+        print(request.POST)
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
@@ -29,14 +37,25 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
-                return redirect('http://127.0.0.1:8000/chat/Test-room/')
+                print(f"Login <{user.username}>")
+                print(f"Key <{generate_encryption_key(password)}>")
+                print(f"Hello <{user.username}>")
+                return redirect('home')
     else:
         form = LoginForm()
 
     return render(request, 'users/login.html', {'form': form})
 
-
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+def generate_encryption_key(password):
+    key = os.urandom(16)
+    hashed_password = hashlib.sha256(password.encode()).digest()[:16]
+    cipher = AES.new(hashed_password, AES.MODE_ECB)
+    encrypted_key = cipher.encrypt(key.ljust(16, b"\0"))
+    encrypted_key_b64 = base64.b64encode(encrypted_key).decode()
+    return encrypted_key_b64
+
